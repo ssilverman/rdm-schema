723cdd227eb383e0a47b3fa04559024e246cdcd6
Peter Newman <peternewman@users.noreply.github.com>
2020-09-12 23:50:52 +0100

    Try validating all files

--- validate-schema.js
+++ validate-schema.js
@@ -1,12 +1,7 @@
+const fs = require("fs");
 const JsonSchema = require("@hyperjump/json-schema");
 
 async function validate(filename) {
-  // Fetch from file
-  const schema = await JsonSchema.get("file:///home/runner/work/rdm-schema/rdm-schema/rdm-schema.json");
-
-  JsonSchema.setShouldMetaValidate(true);
-  JsonSchema.setMetaOutputFormat(JsonSchema.VERBOSE);
-
   const output = await JsonSchema.validate(schema, filename, JsonSchema.VERBOSE);
   console.log(output);
   if (output.valid) {
@@ -17,5 +12,25 @@ async function validate(filename) {
   }
 }
 
-validate("file:///home/runner/work/rdm-schema/rdm-schema/rdm-schema/examples/e1.20/DEVICE_INFO.json");
-validate("file:///home/runner/work/rdm-schema/rdm-schema/rdm-schema/examples/e1.20/IDENTIFY_DEVICE.json");
+//From https://gist.github.com/kethinov/6658166#gistcomment-2037451
+function walkSync(dir) {
+    if (!fs.lstatSync(dir).isDirectory()) return dir;
+
+    return fs.readdirSync(dir).map(f => walkSync(path.join(dir, f))); // `join("\n")`
+}
+
+function validateAllFiles(path) {
+  // Fetch from file
+  const schema = await JsonSchema.get("file:///home/runner/work/rdm-schema/rdm-schema/rdm-schema.json");
+
+  JsonSchema.setShouldMetaValidate(true);
+  JsonSchema.setMetaOutputFormat(JsonSchema.VERBOSE);
+
+  walkSync(path)
+    .filter((entry) => /\.json$/.test(entry))
+    .forEach((entry) => {
+      validate(entry)
+    });
+}
+
+validateAllFiles("./examples");
