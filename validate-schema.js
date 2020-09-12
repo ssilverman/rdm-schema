d863a11c43fb8323fa9441af3d8290739ff87723
Peter Newman <peternewman@users.noreply.github.com>
2020-09-12 23:32:09 +0100

    Validate two files

--- validate-schema.js
+++ validate-schema.js
@@ -1,20 +1,21 @@
 const JsonSchema = require("@hyperjump/json-schema");
 
-async function validate() {
+async function validate(filename) {
   // Fetch from file
   const schema = await JsonSchema.get("file:///home/runner/work/rdm-schema/rdm-schema/rdm-schema.json");
 
   JsonSchema.setShouldMetaValidate(true);
   JsonSchema.setMetaOutputFormat(JsonSchema.VERBOSE);
 
-  const output = await JsonSchema.validate(schema, "file:///home/runner/work/rdm-schema/rdm-schema/rdm-schema/examples/e1.20/DEVICE_INFO.json", JsonSchema.VERBOSE);
+  const output = await JsonSchema.validate(schema, filename, JsonSchema.VERBOSE);
   console.log(output);
   if (output.valid) {
-    console.log("File is valid :-)");
+    console.log("File " + filename + " is valid :-)");
   } else {
-    console.log("File is invalid :-(");
+    console.log("File " + filename + " is invalid :-(");
     process.exitCode = 1
   }
 }
 
-validate();
+validate("file:///home/runner/work/rdm-schema/rdm-schema/rdm-schema/examples/e1.20/DEVICE_INFO.json");
+validate("file:///home/runner/work/rdm-schema/rdm-schema/rdm-schema/examples/e1.20/IDENTIFY_DEVICE.json");
