093878011d1b24d9e708d9d3374f0a293e1b7459
Peter Newman <peternewman@users.noreply.github.com>
2020-09-12 21:03:01 +0100

    Remove awaits

--- validate-schema.js
+++ validate-schema.js
@@ -5,8 +5,8 @@ const schemaJson = {
   "$schema": "https://json-schema.org/draft/2019-09/schema",
 }
 JsonSchema.add(schemaJson);
-const schema = await JsonSchema.get("https://json-schema.org/draft/2019-09/schema");
+const schema = JsonSchema.get("https://json-schema.org/draft/2019-09/schema");
 
 // Example: Specify output format
-const output = await JsonSchema.validate(schema, "foo", JsonSchema.VERBOSE);
+const output = JsonSchema.validate(schema, "foo", JsonSchema.VERBOSE);
 console.log(output);
