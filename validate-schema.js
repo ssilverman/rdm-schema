7b4e231f8001c4c2f11cf2605de90910642a2f5d
Peter Newman <peternewman@users.noreply.github.com>
2020-09-12 21:22:11 +0100

    Try and deal with unhandled promises

--- validate-schema.js
+++ validate-schema.js
@@ -1,10 +1,14 @@
 const JsonSchema = require("@hyperjump/json-schema");
 
-// Fetch from file
-const schema = JsonSchema.get("file:///home/runner/work/rdm-schema/rdm-schema/rdm-schema.json");
+async function validate() {
+  // Fetch from file
+  const schema = await JsonSchema.get("file:///home/runner/work/rdm-schema/rdm-schema/rdm-schema.json");
 
-JsonSchema.setShouldMetaValidate(true);
-JsonSchema.setMetaOutputFormat(JsonSchema.VERBOSE);
+  JsonSchema.setShouldMetaValidate(true);
+  JsonSchema.setMetaOutputFormat(JsonSchema.VERBOSE);
 
-const output = JsonSchema.validate(schema, "file:///home/runner/work/rdm-schema/rdm-schema/rdm-schema/examples/e1.20/DEVICE_INFO.json", JsonSchema.VERBOSE);
-console.log(output);
+  const output = await JsonSchema.validate(schema, "file:///home/runner/work/rdm-schema/rdm-schema/rdm-schema/examples/e1.20/DEVICE_INFO.json", JsonSchema.VERBOSE);
+  console.log(output);
+}
+
+validate();
