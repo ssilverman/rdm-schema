04e917cb229ce04de11b723ebbf96e9f5698a374
Peter Newman <peternewman@users.noreply.github.com>
2020-09-12 23:14:01 +0100

    Try and exit when we get an error

--- validate-schema.js
+++ validate-schema.js
@@ -9,6 +9,12 @@ async function validate() {
 
   const output = await JsonSchema.validate(schema, "file:///home/runner/work/rdm-schema/rdm-schema/rdm-schema/examples/e1.20/DEVICE_INFO.json", JsonSchema.VERBOSE);
   console.log(output);
+  if (output.valid) {
+    console.log("File is valid :-)");
+  } else {
+    console.log("File is invalid :-(");
+    process.exitCode = 1
+  }
 }
 
 validate();
