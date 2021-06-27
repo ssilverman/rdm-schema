4035e593e1ee9604bc2c110d3114bd0b42e25a5d
Peter Newman <peternewman@users.noreply.github.com>
2021-06-27 21:31:56 +0100

    More debugging

--- validate-schema.mjs
+++ validate-schema.mjs
@@ -12,11 +12,13 @@ async function validate(validator, filename) {
 
   // Fetch from file
   const fileContent = await fs.readFile(path.resolve() + filename)
-  console.log("Checking file " + filename);
-/*  const output = validator.validate(JSON.parse(fileContent.toString()))
+  console.log("Checking file " + path.resolve() + filename);
+  console.log("Contents:");
+  console.log(JSON.parse(fileContent.toString()));
+  const output = validator.validate(JSON.parse(fileContent.toString()))
   // const output = await JsonSchema.validate(schema, "file://" + filename, JsonSchema.VERBOSE);
   //  console.log(output);
-  if (output.valid) {
+  /*if (output.valid) {
     console.log("File " + filename + " is valid :-)");
   } else {
     console.log("File " + filename + " is invalid :-(", output.errors); process.exitCode = 1
